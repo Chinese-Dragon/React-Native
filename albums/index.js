85bc4d80124c0516018f805f81eab314d7dd3eee
YingzhengMa <yma004@drury.edu>
2018-06-17 19:40:21 -0700

    added header component

--- albums/index.js
+++ albums/index.js
@@ -1,11 +1,13 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistery, Text } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header.js';
 
 // Create a functional component
 const App = () => (
-  <Text>Hello World</Text>
+  <Header />
 );
 
 // Render component to device
-AppRegistery.registerComponent('albums', () => App);
+// only the root component use AppRegistry
+AppRegistry.registerComponent('albums', () => App);
---
+++ albums/src/components/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => (
+  <Text>Hello World</Text>
+);
+
+export default Header;
