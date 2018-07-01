2913c5b4fa7bd374c299c3fb836502e9e8633e0b
YingzhengMa <yma004@drury.edu>
2018-06-30 20:48:49 -0700

    added header component

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -3,14 +3,16 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers'; //do not need to specify index.js
-
+import { Header } from './components/common';
 // 1: Store holds the states;
 // 2: Provider is the communication glue to React
 // 3: put reducer in store
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText="ollll" />
+      </View>
     </Provider>
   );
 };
