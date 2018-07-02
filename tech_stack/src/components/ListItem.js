c7958a9ca2388af0fba784641d5084effa2cc925
YingzhengMa <yma004@drury.edu>
2018-07-01 20:04:20 -0700

    added selectionReducer and connect ListItem component to use it to determine which section is tapped on and conditionally render some texts

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
 
 class ListItem extends Component {
+
+  showContent() {
+    const libraryTitle = this.props.library.item.title;
+    const libraryId = this.props.library.item.id;
+    const selectedLibraryId = this.props.selectedLibraryId;
+    const { titleStyle } = styles;
+
+    console.log(selectedLibraryId);
+    console.log(libraryId);
+
+    if (selectedLibraryId === libraryId) {
+      return <Text style={titleStyle}>{`${libraryTitle} selected!!!`}</Text>;
+    }
+
+    return <Text style={titleStyle}>{libraryTitle}</Text>;
+  }
   //const { titleStyle } = styles;
   render() {
-    const { titleStyle } = styles;
     return (
       <CardSection>
-        <Text style={titleStyle}>{this.props.library.item.title}</Text>
+        {this.showContent()};
       </CardSection>
     );
   }
@@ -17,8 +33,13 @@ class ListItem extends Component {
 const styles = {
   titleStyle: {
     fontSize: 18,
-    paddingLeft:15
+    paddingLeft: 15
   }
 };
 
-export default ListItem;
+const mapStateToProps = (state) => {
+  // will add { selectedLibraryId: state.selectedLibraryId } to props
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
+export default connect(mapStateToProps)(ListItem);
---
+++ tech_stack/src/reducers/SelectionReducer.js
@@ -0,0 +1,4 @@
+export default () => {
+  // return single item from reducer need not to be underfined
+  return 0;
+};
